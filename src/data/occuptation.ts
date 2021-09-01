600ec13f624d877c5ac42effc3a75f54f40c5c8d
unknown <rory.malone@hotmail.com>
2021-09-01 20:17:19 +0100

    feat(update): add new occupation and update assets

--- src/data/occuptation.ts
+++ src/data/occuptation.ts
@@ -1,11 +1,19 @@
 import arm from '../assets/images/arm.png'
+import sky from '../assets/images/sky.png'
 import uel from '../assets/images/uel.png'
 import bt from '../assets/images/bt.png'
 
 const OCCUPATION = [
     {
+        location: 'Sky',
+        timeframe: '2021 - Present',
+        imageSrc: sky,
+        title: 'Senior Developer',
+        text: 'Currently owning development of the interface for high value customers to access their downloads. I am responsible for designing and implementing the end to end solutions.'
+    },
+    {
         location: 'Arm Holdings ltd.',
-        timeframe: '2019 - Present',
+        timeframe: '2019 - 2021',
         imageSrc: arm,
         title: 'Senior Full Stack Web Developer',
         text: 'Currently owning development of the interface for high value customers to access their downloads. I am responsible for designing and implementing the end to end solutions.'
